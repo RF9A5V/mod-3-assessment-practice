0f61c4bec02d8e9d2deb87c557c9f5bd62432e19
Steven Zhang <steven@pursuit.org>
2022-11-30 18:43:53 -0500

    base work

--- src/App.js
+++ src/App.js
@@ -13,9 +13,37 @@ function App() {
     fetch(PRIZE_API).then(resp => resp.json()).then(console.log)
   }, [])
 
+  const [dropdownActive, setDropdown] = useState(false);
+
+  // Note, you'll have to move some of this content to the PrizeContainer when we bring in routing
+  // We should be able to go to /prizes/chemistry to find all the Nobel Prize winners for chemistry, as an example.
+
   return (
     <div className="App">
+      <nav>
+        <h4>Nobel</h4>
+        <div className="dropdown-container">
+          <span onMouseEnter={() => setDropdown(true)} onMouseLeave={() => setDropdown(false)}>
+            Categories
+          </span>
+          {
+            dropdownActive ? (
+              <ul className="dropdown">
+                { /* Should have a link to each of the prize categories */ }
+                <li>Test Entry</li>
+                <li>Test Entry</li>
+                <li>Test Entry</li>
+                <li>Test Entry</li>
+              </ul>
+            ) : (
+              null
+            )
+          }
+          
+        </div>
+      </nav>
       <h3>Nobel Prizes</h3>
+      { /* Should have a dropdown that lets you filter prizes based on the year */ }
       { /* For each of the prizes you get back from the API, render a new prize component */}
     </div>
   );
---
+++ src/components/PrizeContainer.jsx
@@ -0,0 +1,9 @@
+// We want this component to render our prizes and to potentially sort our prize winners
+// We should be able to sort our winners by year and last name (if multiple winners for a single award,
+// order the winners for that one award, then use the first winner in the list for your sort value.
+
+function PrizeContainer() {
+  return (
+    <div></div>
+  )
+}
