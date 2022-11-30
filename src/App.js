d9253e3ce6d08da929340a0ee26160c41dab8f62
Steven Zhang <steven@pursuit.org>
2022-11-30 18:23:58 -0500

    Base repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const PRIZE_API = "/prize.json";
 
 function App() {
+
+  // Create a state variable to hold prize data
+
+  // Get the cards from the PRIZE_API endpoint and put them into the state variable you created above
+  useEffect(() => {
+    fetch(PRIZE_API).then(resp => resp.json()).then(console.log)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Nobel Prizes</h3>
+      { /* For each of the prizes you get back from the API, render a new prize component */}
     </div>
   );
 }
---
+++ src/components/Prize.jsx
@@ -0,0 +1,9 @@
+function Prize() {
+  return (
+    <div>
+      <span>Prize details go here</span>
+      { /* Clicking this button should delete the given card */ }
+      <button>x</button>
+    </div>
+  )
+}
